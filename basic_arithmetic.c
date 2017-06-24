3961598664ee5db49ed28f1c3713ecfa78fae026
NikhilMara <nikhilreddymara@gmail.com>
2017-06-24 12:57:24 +0200

    basic arithmetic

--- basic_arithmetic.c
+++ basic_arithmetic.c
@@ -18,3 +18,12 @@ void main(){
      printf(" the modulus of %d and %d is %d \n",a,c,mod);
 }
 
+/*
+The division of 9 and 2.000000 is 4.500000
+The division of 9 and 10 is 0
+The sum of 9 and 10 is 19
+The multiplication of 9 and 10 is 90
+The difference of 9 and 10 is -1
+The modulus of 9 and 10 is 9
+	 
+*/
