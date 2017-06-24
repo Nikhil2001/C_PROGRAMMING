70db898f418f18bf6eb64686bdc2892ec821dcc0
NikhilMara <nikhilreddymara@gmail.com>
2017-06-24 10:13:20 +0200

    Hello World !

--- hello_world.c
+++ hello_world.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*the above statements are called preprocessor directives.
+for example: printf is a function whose definition(what it does to the arguments passed and return code inside printf block)
+can be found in stdio.h header file
+summary: include to use printf like inbuilt functions
+
+
+*/
+//main() is a function where compiler starts execution
+//since its function it take  may take input arguments and return something. here main takes nothing but returns int.
+
 int main(){
 
-	printf("Hello,World!");
+    printf("Hello,World!");  //terminate each statement with semi-colon
     return 0;
 }
 
