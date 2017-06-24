6c33ddbbeb7bdfdaaa320425df27efcb32522a8b
NikhilMara <nikhilreddymara@gmail.com>
2017-06-24 10:50:08 +0200

    printf and placeholder usage

--- printf.c
+++ printf.c
@@ -7,7 +7,7 @@ void main(){
 	
 	printf("This is line 1");
 	printf("\nThis is line 2\n");
-	printf("%s\n","nikhil"); // to print a string , the placeholder is %d
+	printf("%s\n","nikhil"); // to print a string , the placeholder is %s
 	printf("%d\n",10); // to print integer
 	printf("%c\n",'n'); //to print a string
 	printf("%f",2.1); // to print a decimal number  or float
