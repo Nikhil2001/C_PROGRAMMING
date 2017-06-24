ee7afb4b3d50d6fde2706615d95541d211e226ca
NikhilMara <nikhilreddymara@gmail.com>
2017-06-24 10:42:21 +0200

    printf and placeholders usage

---
+++ printf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// how to print different data types using printf
+void main(){
+	
+	printf("This is line 1");
+	printf("\nThis is line 2\n");
+	printf("%s\n","nikhil"); // to print a string , the placeholder is %d
+	printf("%d\n",10); // to print integer
+	printf("%c\n",'n'); //to print a string
+	printf("%f",2.1); // to print a decimal number  or float
+	
+}
+
+/*output
+This is line1
+This is line 2
+nikhil
+10
+n
+2.100000
+
+*/
