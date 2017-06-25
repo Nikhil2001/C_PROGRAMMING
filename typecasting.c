efecf956f6d4ff4151cd78b43ce9fb52eddb139b
NikhilMara <nikhilreddymara@gmail.com>
2017-06-25 15:33:43 +0200

    casting example

---
+++ typecasting.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main(){
+	int  k = 99,m;
+	char i='c';
+	int c = (char) k; // type casting int to char, 99 is converted to ascii equivalent of c  
+	printf("%c \n",c); // c 
+	m=(int) i;
+	printf("%d \n",m); // 99 char i value is c convered into ascii value 99 using int casting
+	
+	
+	
+}
+//output: The area of circle is 78
